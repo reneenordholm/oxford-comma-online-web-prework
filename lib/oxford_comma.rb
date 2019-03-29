5f65c614c570055d7761b2e5950469d946b27a60
Renee Nordholm <reneenordholm@gmail.com>
2019-03-29 11:18:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    return array.join(", ").insert(-2, "and")
+    return array.join(", ").insert(-2, "and").chomp
   else array.length > 3
     return array.insert(-2, "and").join(", ")
 end
