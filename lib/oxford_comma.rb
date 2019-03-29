8defaca2dda308c896b51496aae59274b59ef745
Renee Nordholm <reneenordholm@gmail.com>
2019-03-29 11:30:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    return array.join
+     array.join
   elsif array.length == 2
-    return array.join(" and ")
+     array.join(" and ")
   elsif array.length == 3
-    return array.insert(-2, "and").join(", ")
+     array.insert(-2, "and").join(", ")
   else array.length > 3
-    return array.insert(-2, "and").join(", ")
+     array.insert(-2, "and").join(", ")
 end
 end
