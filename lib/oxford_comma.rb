5538a0aece3a53a2014e6fc7d96fd0e7aa5aceda
Renee Nordholm <reneenordholm@gmail.com>
2019-03-29 11:08:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    return array.insert(-2, "and").join(", ").chomp
+  else array.length > 3
+    return array.insert(-2, "and").join(", ")
+end
 end
