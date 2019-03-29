a886314d8bace5d313d9293720fe005794e64b81
Renee Nordholm <reneenordholm@gmail.com>
2019-03-29 11:21:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    return array.insert(-2, "and").split
+    return array.insert(-2, "and")
   else array.length > 3
     return array.insert(-2, "and").join(", ")
 end
